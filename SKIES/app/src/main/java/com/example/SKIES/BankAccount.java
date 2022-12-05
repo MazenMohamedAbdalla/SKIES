32ff87f02e8745eb6af197e9f911514487288ac2
Mazen M. Abdalla <Mazen_Mohamed2022@outlook.com>
2022-12-05 23:46:56 +0200

    Added views of Signin/up recover password track flight status

---
+++ SKIES/app/src/main/java/com/example/SKIES/BankAccount.java
@@ -0,0 +1,14 @@
+package com.example.SKIES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BankAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_account);
+    }
+}
---
+++ SKIES/app/src/main/java/com/example/SKIES/CheckStatus.java
@@ -0,0 +1,14 @@
+package com.example.SKIES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_status);
+    }
+}
